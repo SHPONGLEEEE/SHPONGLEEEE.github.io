ebe6177d9f2ecd85da7b878ff868fa41310d0589
SHPONGLEEEE <121955385+SHPONGLEEEE@users.noreply.github.com>
2023-03-01 15:27:44 +0100

    Add files via upload

---
+++ silnik.js
@@ -0,0 +1,46 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+// wczytywanie obrazków
+
+loadSprite("tlo","Kartkaswiateczna.png")
+loadSprite("obrazek","renifer.png")
+loadSound("muzyka", "Cicha noc.mp3")
+
+// add a piece of text at position (120, 80)
+add([
+    text("hello"),
+    pos(120, 80),
+]);
+
+add([
+    sprite("tlo"),
+    pos(0,0)
+])
+const renifer = add([
+    sprite("obrazek"),
+    pos(230,130)
+])
+ add([
+    play("muzyka")
+ ])
+var yMinus = 1;
+var xMinus = 1;
+onUpdate(()=>{
+    if(renifer.pos.x == 230)
+    {
+        xMinus = -1;
+        yMinus = -1;
+    }
+    if(renifer.pos.x == 100)
+    {
+        xMinus = 1;
+        yMinus = 1;
+    }
+    renifer.pos.x=renifer.pos.x += xMinus;
+    renifer.pos.y=renifer.pos.y += yMinus;
+    
+})
+
